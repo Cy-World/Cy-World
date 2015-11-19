dfa4f89df8e8fe0684df71d8c60de6c72ca774c5
shigure242 <shigure242@yahoo.co.jp>
2015-11-19 21:32:05 +0900

    [modify]DBHelperの処理を変更しました
    DBHelperは与えられたSQL文を実行するだけの、本来あるべき機能に修正しました
    
    Model側でOpenDBを実行し、SQL文を発行してください
    処理後は(必ず)CloseDBを実行し、DBを閉じてください

--- src/cyworld/controller/RegisterServlet.java
+++ src/cyworld/controller/RegisterServlet.java
@@ -6,35 +6,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import cyworld.model.DBHelper;
 import cyworld.model.User;
 
-/**
- * Servlet implementation class RegisterServlet
- */
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public RegisterServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
-			System.out.println("aaaaaa");
+			System.out.println("POST start");
 			request.setCharacterEncoding("UTF-8");
 			String name = request.getParameter("username");
 			String address = request.getParameter("email");
 			String passwd = request.getParameter("passwd");
 			User user = new User();
-			user.createUser(name,address,passwd);
+			user.createUser(name, address, passwd);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -42,7 +33,6 @@ public class RegisterServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("GET!!!!!!!!!!!!!!!!");
 		doGet(request, response);
 	}
 
--- src/cyworld/model/User.java
+++ src/cyworld/model/User.java
@@ -1,5 +1,8 @@
 package cyworld.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User {
 	private String name;
 	private String address;
@@ -23,9 +26,30 @@ public class User {
 	}
 
 	public void createUser(String name, String address, String passwd) {
+		String sql = String.format("INSERT INTO User VALUES('00005','%s','%s','%s','0','0');", passwd, address, name);
 		DBHelper dbHelper = new DBHelper();
-		dbHelper.register(name, address, passwd);
-		dbHelper.select();
+		dbHelper.openDB();
+		dbHelper.insertSQL(sql);
+		dbHelper.closeDB();
+
+		// TEST RUN---
+		sql = "SELECT * from User";
+		dbHelper.openDB();
+		ResultSet rs = dbHelper.selectSQL(sql);
+		try {
+			while (rs.next()) {
+				System.out.println("------------------------------------");
+				System.out.println("UserID : " + rs.getString("UserID"));
+				System.out.println("Name : " + rs.getString("UserName"));
+				System.out.println("Passwd : " + rs.getString("Password"));
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			dbHelper.closeDB();
+		}
+		dbHelper.closeDB();
+		// TEST RUN---
 	}
 
 	public void updateUser() {
