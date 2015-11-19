0becef049d55f62285ee54ae3a736e9548313f12
shigure242 <shigure242@yahoo.co.jp>
2015-11-19 19:50:44 +0900

    [modify]新規登録機能のサンプルが完成しました
    現状だと、UserIDが動的にふられていないので、インサートする際は必ず再デプロイをかけてください

--- src/cyworld/model/User.java
+++ src/cyworld/model/User.java
@@ -1,5 +1,35 @@
 package cyworld.model;
 
 public class User {
+	private String name;
+	private String address;
+	private String passwd;
+	private String imgPath;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public String getImgPath() {
+		return imgPath;
+	}
+
+	public void createUser(String name, String address, String passwd) {
+		DBHelper dbHelper = new DBHelper();
+		dbHelper.register(name, address, passwd);
+		dbHelper.select();
+	}
+
+	public void updateUser() {
+
+	}
 
 }
