480de8e44cd2917662f5e5dd079c503ab5e68026
raimu <raimu@RaimuUbuntu>
2015-12-07 11:01:55 +0900

    <modify>androidLoginServletのprintするデータを加筆
    trueのあとにログイン中のユーザーデータをStringで送信するように

--- src/cyworld/model/RoomBean.java
+++ src/cyworld/model/RoomBean.java
@@ -46,37 +46,64 @@ public class RoomBean {
 	}
 	
 	public static ArrayList<RoomBean> privateRoomView(String address){
-		ArrayList<RoomBean> roomData = new ArrayList<>();
-		String sql = String.format("SELECT RoomName,Comment FROM "
-				+ "Room JOIN JoinInfo ON Room.RoomID = JoinInfo.RoomID"
-				+ "JOIN User ON User.UserID = JoinInfo.UserID"
-				+ " WHERE MailAddress=%s AND Flag = '1'", address);
-		DBHelper helper = new DBHelper();
-		helper.openDB();
-		
-		ResultSet rs = helper.selectSQL(sql);
-		
-		try {
-			while(rs.next()){
-				RoomBean bean = new RoomBean(rs.getString("roomID"),
-											 rs.getString("RoomName"), 
-						                     rs.getString("Comment"), 
-						                     (Byte)null);
-				roomData.add(bean);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		helper.closeDB();
-		return roomData;
+//		ArrayList<RoomBean> roomData = new ArrayList<>();
+//		String sql = String.format("SELECT RoomName,Comment FROM "
+//				+ "Room JOIN JoinInfo ON Room.RoomID = JoinInfo.RoomID"
+//				+ "JOIN User ON User.UserID = JoinInfo.UserID"
+//				+ " WHERE MailAddress=%s AND Flag = '1'", address);
+//		DBHelper helper = new DBHelper();
+//		helper.openDB();
+//		
+//		ResultSet rs = helper.selectSQL(sql);
+//		
+//		try {
+//			while(rs.next()){
+//				RoomBean bean = new RoomBean(rs.getString("roomID"),
+//											 rs.getString("RoomName"), 
+//						                     rs.getString("Comment"), 
+//						                     (Byte)null);
+//				roomData.add(bean);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		helper.closeDB();
+//		return roomData;
+		return getRoomList(1);
 	}
 	
 	public static ArrayList<RoomBean> publicRoomView(){
+//		ArrayList<RoomBean> roomData = new ArrayList<>();
+//		String sql ="SELECT RoomName,Comment FROM "
+//				+ "Room JOIN JoinInfo ON Room.RoomID = JoinInfo.RoomID"
+//				+ " WHERE Flag = '0'";
+//		DBHelper helper = new DBHelper();
+//		helper.openDB();
+//		
+//		ResultSet rs = helper.selectSQL(sql);
+//		
+//		try {
+//			while(rs.next()){
+//				RoomBean bean = new RoomBean(rs.getString("roomID"),
+//											 rs.getString("RoomName"), 
+//						                     rs.getString("Comment"), 
+//						                     (Byte)null);
+//				roomData.add(bean);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		helper.closeDB();
+		return getRoomList(0);
+	}
+	private static ArrayList<RoomBean> getRoomList(int flag){
+		//0=public 1=private
 		ArrayList<RoomBean> roomData = new ArrayList<>();
 		String sql ="SELECT RoomName,Comment FROM "
 				+ "Room JOIN JoinInfo ON Room.RoomID = JoinInfo.RoomID"
-				+ " WHERE Flag = '0'";
+				+ " WHERE Flag = '"+flag+"'";
 		DBHelper helper = new DBHelper();
 		helper.openDB();
 		
@@ -97,8 +124,6 @@ public class RoomBean {
 		helper.closeDB();
 		return roomData;
 	}
-
-
 	public String getRoomID() {
 		return roomID;
 	}
