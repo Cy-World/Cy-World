bac482f362e2db780e492e977076845bf4af4e4d
shigure242 <x13g029@chiba-fjb.ac.jp>
2016-01-20 16:23:02 +0900

    tmp

--- src/cyworld/controller/LogoutServlet.java
+++ src/cyworld/controller/LogoutServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LogoutServlet
+ * Servlet implementation class LogoutServlet.
  */
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
